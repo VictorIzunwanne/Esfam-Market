242640fa11a2ee995ec356d1f7ad0c7db94ea22b
Victor <victorizunwanneweb@gmail.com>
2025-07-24 14:34:52 +0100

    jsdepw

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: "https://esfam-market.netlify.app",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    metods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
